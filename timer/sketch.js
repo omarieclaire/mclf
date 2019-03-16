2f66a6ccfbbaacb7594ab36a5ffd53b1af5c7dfa
omarieclaire <marieflanagan@gmail.com>
2019-03-16 16:46:25 -0400

    timer

--- timer/sketch.js
+++ timer/sketch.js
@@ -1,3 +1,12 @@
+
+//start pretty var
+const canvas = 1080;
+let cZero;
+let cTick;
+let colours = [];
+//end pretty var
+
+
 var timeleft = 10;
 var ding;
 var interval = false;
@@ -5,7 +14,9 @@ var interval = false;
 // Convert seconds to min:sec
 function convertSeconds(s) {
 	var min = floor(s / 60);
+	//modulus gives you the remainder of the division
 	var sec = s % 60;
+	//nf is number format, a p5 thing
 	return nf(min, 2) + ':' + nf(sec, 2);
 }
 
@@ -15,6 +26,7 @@ function preload() {
 
 // Set value of the DOM element
 function timerText(text) {
+	//select allows me to access an html element on the page
 	var timer = select('#timer');
 	timer.html(text);
 }
@@ -26,7 +38,22 @@ function stopTimer() {
 }
 
 function setup() {
-	noCanvas();
+	//start pretty setup
+	 createCanvas(canvas, canvas);
+	 colorMode(RGB, 255, 255, 255, 1);
+
+	 cZero = PI + HALF_PI;
+	 cTick = TWO_PI / 60;
+
+	 colours = [
+	   color('#083355'),
+	   color('#74B8EE'),
+	   color('#74B8EE'),
+	   color('#5689B1'),
+	   color('#3D617E'),
+	  ];
+//end pretty setup
+
 
 	// Process URL ?minute=
 	var params = getURLParams();
@@ -35,10 +62,52 @@ function setup() {
 		timeleft = min * 60;
 	}
 
+//start pretty draw
+
+function draw() {
+  background(colours[0]);
+
+  const d = new Date();
+	drawCircle(width * 0.8, Math.ceil(((d.getMilliseconds() + 1) / 100) * 6), colours[1], true);
+	drawCircle(width * 0.6, d.getSeconds(), colours[2], false);
+	drawCircle(width * 0.4, d.getMinutes(), colours[3], false);
+  let hour = d.getHours();
+  if (hour > 12) {
+    hour -= 12;
+  }
+  hour += (d.getMinutes() / 60);
+  hour *= 5 - 4;
+  drawCircle(width * 0.2, hour, colours[4], false);
+}
+
+function drawCircle(size, value, colour, fade) {
+  // Fade?
+  if (fade === true) {
+    const alpha = map(value, 1, 60, 1, 0);
+    stroke(70, 83, 135, alpha);
+  } else {
+    stroke(colour);
+  }
+
+  noFill();
+  strokeWeight(100.0);
+  strokeCap(ROUND);
+
+  // Draw full ellipse if loop is complete
+  if (value === 0) {
+    ellipse(width / 2, height / 2, size, size);
+  } else {
+    arc(width / 2, height / 2, size, size, cZero, (cTick * value) - HALF_PI);
+  }
+}
+
+//end pretty draw
+
+
 	// Set initial value for DOM element
 	timerText(convertSeconds(timeleft) + ' (paused)');
 
-	// Timer interval
+	// Timer interval- function that runs every X amount of time
 	function timeIt() {
 		timeleft--;
 		timerText(convertSeconds(timeleft));
@@ -82,6 +151,7 @@ function setup() {
 			if(timeleft <= 0) return alert('No time set!');
 
 			timerText(convertSeconds(timeleft));
+			//setinterval allows me to set sonething to happen every so often, here every 1000 miliseconds
 			interval = setInterval(timeIt, 1000);
 		}
 		else
