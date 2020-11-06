ee6e8980c82c2fe4b4b640eae258ec683ec77e79
omarieclaire <marieflanagan@gmail.com>
2020-11-06 17:07:13 -0500

    scrolltrigger

--- moodring/moodring/scrollAnimations.js
+++ moodring/moodring/scrollAnimations.js
@@ -1,7 +1,7 @@
 gsap.registerPlugin(ScrollTrigger);
 ScrollTrigger.defaults({
   toggleActions: "restart pause reverse pause", // onEntry onLeaving onReEntry
-  markers: true,
+  // markers: true,
 });
 
 function init() {
