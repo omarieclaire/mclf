99666694f23e0b6ada338afaa2d5ca71b719d3b4
marie <marie@MacBook-Pro.localdomain>
2021-02-13 14:33:16 -0500

    Add touching

--- softie/js/play.js
+++ softie/js/play.js
@@ -1,83 +1,102 @@
 function windowOnLoad() {
+  var currBtn;
 
+  const song1 = new Audio("audio/love.mp3");
+  const song2 = new Audio("audio/ask.mp3");
+  const song3 = new Audio("audio/face.mp3");
+  const song4 = new Audio("audio/hey.mp3");
+  const song5 = new Audio("audio/numb.mp3");
+  const song6 = new Audio("audio/why.mp3");
 
-var currBtn;
+  const songs = [song1, song2, song3, song4, song5, song6];
 
-const song1 = new Audio("audio/love.mp3");
-const song2 = new Audio("audio/ask.mp3");
-const song3 = new Audio("audio/face.mp3");
-const song4 = new Audio("audio/hey.mp3");
-const song5 = new Audio("audio/numb.mp3");
-const song6 = new Audio("audio/why.mp3");
+  var video = document.getElementById("video");
+  var source = document.createElement("source");
+  video.appendChild(source);
 
-const songs = [song1, song2, song3, song4, song5, song6];
+  const btn1 = document.getElementById("btn1");
+  const btn2 = document.getElementById("btn2");
+  const btn3 = document.getElementById("btn3");
+  const btn4 = document.getElementById("btn4");
+  const btn5 = document.getElementById("btn5");
+  const btn6 = document.getElementById("btn6");
 
-var video = document.getElementById('video');
-var source = document.createElement('source');
-video.appendChild(source);
+  btn1.addEventListener(
+    "click",
+    function () {
+      updateBtnStyle(btn1);
+      source.setAttribute("src", "img/v1.mp4");
+      video.load();
+      playSong(song1);
+    },
+    false
+  );
+  btn2.addEventListener(
+    "click",
+    function () {
+      updateBtnStyle(btn2);
+      source.setAttribute("src", "img/v2.mp4");
+      video.load();
+      playSong(song2);
+    },
+    false
+  );
+  btn3.addEventListener(
+    "click",
+    function () {
+      updateBtnStyle(btn3);
+      source.setAttribute("src", "img/v3.mp4");
+      video.load();
+      playSong(song3);
+    },
+    false
+  );
+  btn4.addEventListener(
+    "click",
+    function () {
+      updateBtnStyle(btn4);
+      source.setAttribute("src", "img/v4.mp4");
+      video.load();
+      playSong(song4);
+    },
+    false
+  );
+  btn5.addEventListener(
+    "click",
+    function () {
+      updateBtnStyle(btn5);
+      source.setAttribute("src", "img/v5.mp4");
+      video.load();
+      playSong(song5);
+    },
+    false
+  );
+  btn6.addEventListener(
+    "click",
+    function () {
+      updateBtnStyle(btn6);
+      source.setAttribute("src", "img/v6.mp4");
+      video.load();
+      playSong(song6);
+    },
+    false
+  );
 
-const btn1 = document.getElementById("btn1")
-const btn2 = document.getElementById("btn2")
-const btn3 = document.getElementById("btn3")
-const btn4 = document.getElementById("btn4")
-const btn5 = document.getElementById("btn5")
-const btn6 = document.getElementById("btn6")
+  function updateBtnStyle(clickedBtn) {
+    // remove the class from the old button (which is the "current" button)
+    if (currBtn !== undefined) {
+      currBtn.classList.remove("currBtn");
+    }
+    currBtn = clickedBtn; // update the "current" button to the most recently clicked button
+    clickedBtn.classList.add("currBtn");
+  }
 
-btn1.addEventListener("click", function(){
-	updateBtnStyle(btn1);
-	source.setAttribute('src', 'img/v1.mp4');
-	video.load();
-    playSong(song1);
-}, false);
-btn2.addEventListener("click", function(){
-	updateBtnStyle(btn2);
-	source.setAttribute('src', 'img/v2.mp4');
-	video.load();
-    playSong(song2);
-}, false);
-btn3.addEventListener("click", function(){
-	updateBtnStyle(btn3);
-	source.setAttribute('src', 'img/v3.mp4');
-	video.load();
-    playSong(song3);
-}, false); 
-btn4.addEventListener("click", function(){
-	updateBtnStyle(btn4);
-	source.setAttribute('src', 'img/v4.mp4');
-	video.load();
-    playSong(song4);
-}, false);
-btn5.addEventListener("click", function(){
-	updateBtnStyle(btn5);
-	source.setAttribute('src', 'img/v5.mp4');
-	video.load();
-    playSong(song5);
-}, false);
-btn6.addEventListener("click", function(){
-	updateBtnStyle(btn6);
-	source.setAttribute('src', 'img/v6.mp4');
-	video.load();
-    playSong(song6);
-}, false); 
-
-
-function updateBtnStyle(clickedBtn){
-	// remove the class from the old button (which is the "current" button)
-	if (currBtn !== undefined){
-		currBtn.classList.remove("currBtn");
-	}
-	currBtn = clickedBtn; 	// update the "current" button to the most recently clicked button
-	clickedBtn.classList.add("currBtn");
+  function playSong(song) {
+    for (i = 0; i < songs.length; i++) {
+      songs[i].pause();
+    }
+    song.play();
+  }
 }
-
-function playSong(song){
-	for(i=0; i<songs.length; i++) {
-		songs[i].pause();
-	}
-	song.play();
-}
-}
-
 
 window.addEventListener("load", windowOnLoad);
-
