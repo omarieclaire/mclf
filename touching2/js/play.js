c4082b7c4de27a1b8b08b8b1f1dbac4ce6f10d20
marie <marie@MacBook-Pro.localdomain>
2021-02-18 16:11:08 -0500

    v2

--- touching2/js/play.js
+++ touching2/js/play.js
@@ -1,52 +1,65 @@
 function windowOnLoad() {
 
+  const song1 = new Audio("audio/waterfall.mp3");
+  const songs = [song1];
+
+  var srockpond = new Audio('audio/rockpond.mp3');
+  var sfacewindow = new Audio('audio/facewindow.mp3');
+  var sspoonbatter = new Audio('audio/spoonbatter.mp3');
+  var sweightscale = new Audio('audio/weightscale.mp3');
+  var sholehome = new Audio('audio/holehome.mp3');
+  var sdarkeye = new Audio('audio/darkeye.mp3');
+  var shandsilk = new Audio('audio/handsilk.mp3');
+  var sno = new Audio('audio/no.mp3');
+
+  const sounds = [srockpond, sfacewindow, sspoonbatter, sweightscale, sholehome, sdarkeye, shandsilk, sno]
+
+
   var touchGen = [{
     word: 'Rock',
     video: 'img/ot1.mp4',
-    sound: 'audio/rockpond.mp3',
+    sound: srockpond,
     desc: 'I am touching you like a rock skates across a frozen pond.'
   }, {
     word: 'Face',
     video: 'img/ot2.mp4',
-    sound: 'audio/facewindow.mp3',
+    sound: sfacewindow,
     desc: 'I feel your touch like a face in a window.'
-  }, {
-    word: 'Spoon',
-    video: 'img/ot3.mp4',
-    sound: 'audio/spoonbatter.mp3',
-    desc: 'I am touching you like a spoon stirs thick batter.'
-  }, {
+  }, 
+  // {
+  //   word: 'Spoon',
+  //   video: 'img/ot3.mp4',
+  //   sound: sspoonbatter,
+  //   desc: 'I am touching you like a spoon stirs thick batter.'
+  // }, 
+  {
     word: 'Weight',
     video: 'img/ot4.mp4',
-    sound: 'audio/weightscale.mp3',
+    sound: sweightscale,
     desc: 'I feel your touch like a weight presses on a scale.'
   }, {
     word: 'Hole',
     video: 'img/ot5.mp4',
-    sound: 'audio/holehome.mp3',
+    sound: sholehome,
     desc: 'I am touching you like the hole touches the place it makes its home.'
   }, {
     word: 'Dark',
     video: 'img/ot6.mp4',
-    sound: 'audio/darkeye.mp3',
+    sound: sdarkeye,
     desc: 'I feel your touch like the darkness touches an eye.'
   }, {
     word: 'Hand',
     video: 'img/ot7.mp4',
-    sound: 'audio/handsilk.mp3',
+    sound: shandsilk,
     desc: 'I am touching you like a rough hand grazes across silk.'
   }, {
     word: 'No',
     video: 'img/ot8.mp4',
-    sound: 'audio/no.mp3',
+    sound: sno,
     desc: 'I do not feel your touch.'
   }, ];
 
   var usedTouches = [];
-
-  const song1 = new Audio("audio/waterfall.mp3");
-  const songs = [song1];
-
   var video = document.getElementById("video");
   const videos = [video];
 
@@ -56,14 +69,14 @@ function windowOnLoad() {
   const button = document.getElementById("words");
 
   function randomSelector() {
-    var dronegenLength = touchGen.length;
+    var touchGenLength = touchGen.length;
 
-    var randomNumber = Math.floor(Math.random() * dronegenLength);
+    var randomNumber = Math.floor(Math.random() * touchGenLength);
 
     var newword = touchGen[randomNumber].word;
     var newdesc = touchGen[randomNumber].desc;
 
-    var newsound = new Audio('audio/handsilk.mp3');
+    var newsound = touchGen[randomNumber].sound;
     // var newsound = touchGen[randomNumber].sound;
     var newvideo = touchGen[randomNumber].video;
 
@@ -78,7 +91,7 @@ function windowOnLoad() {
     usedTouches.push(touchGen[randomNumber]);
     touchGen.splice(randomNumber, 1);
 
-    if (dronegenLength === 1) {
+    if (touchGenLength === 1) {
       touchGen = [...usedTouches]; // copy usedTouches into touchGen
       usedTouches = []; //empty bucket - so next time we don't have doubles
     }
