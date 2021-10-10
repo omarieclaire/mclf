43d4adb069aafc7c402312262b737948f5eb1a3c
Marie Claire LeBlanc Flanagan <marieflanagan@gmail.com>
2021-10-10 00:20:23 -0400

    timer updates

--- vidtimer/app.js
+++ vidtimer/app.js
@@ -1,10 +1,8 @@
 const song = document.querySelector(".song");
 const play = document.querySelector(".play");
-const replay = document.querySelector(".replay");
+// const replay = document.querySelector(".replay");
 const outline = document.querySelector(".moving-outline circle");
 const video = document.querySelector(".vid-container video");
-//Sounds
-const sounds = document.querySelectorAll(".sound-picker button");
 //Time Display
 const timeDisplay = document.querySelector(".time-display");
 const outlineLength = outline.getTotalLength();
@@ -14,44 +12,41 @@ let fakeDuration = 60;
 
 outline.style.strokeDashoffset = outlineLength;
 outline.style.strokeDasharray = outlineLength;
-timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-  fakeDuration % 60
-)}`;
 
-sounds.forEach(sound => {
-  sound.addEventListener("click", function() {
-    song.src = this.getAttribute("data-sound");
-    video.src = this.getAttribute("data-video");
-    checkPlaying(song);
-  });
-});
+// timeDisplay.textContent = Math.floor(fakeDuration % 60);
 
-play.addEventListener("click", function() {
+
+
+
+play.addEventListener("click", function () {
   checkPlaying(song);
 });
 
-replay.addEventListener("click", function() {
-    restartSong(song);
-  });
+// replay.addEventListener("click", function () {
+//   restartSong(song);
+// });
 
-const restartSong = song =>{
-    let currentTime = song.currentTime;
-    song.currentTime = 0;
-    console.log("ciao")
+const restartSong = song => {
+  let currentTime = song.currentTime;
+  song.currentTime = 0;
+  console.log("ciao")
 }
 
 timeSelect.forEach(option => {
-  option.addEventListener("click", function() {
+  option.addEventListener("click", function () {
+    song.src = this.getAttribute("data-sound");
+    video.src = this.getAttribute("data-video");
+    checkPlaying(song);
     fakeDuration = this.getAttribute("data-time");
-    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-      fakeDuration % 60
-    )}`;
+    // timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+    //   fakeDuration % 60
+    // )}`;
   });
 });
 
 const checkPlaying = song => {
   if (song.paused) {
-    song.volume = 0.001;
+    song.volume = 0.1;
     song.play();
     video.play();
     play.src = "./svg/pause.svg";
@@ -62,12 +57,13 @@ const checkPlaying = song => {
   }
 };
 
-song.ontimeupdate = function() {
+song.ontimeupdate = function () {
   let currentTime = song.currentTime;
   let elapsed = fakeDuration - currentTime;
   let seconds = Math.floor(elapsed % 60);
   let minutes = Math.floor(elapsed / 60);
-  timeDisplay.textContent = `${minutes}:${seconds}`;
+  //number time
+  // timeDisplay.textContent = `${minutes}:${seconds}`;
   let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
   outline.style.strokeDashoffset = progress;
 
