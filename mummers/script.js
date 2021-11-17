dbc508dc5e06f54c1e3914773fc07f7c130372af
Marie Claire LeBlanc Flanagan <marieflanagan@gmail.com>
2021-11-17 01:15:55 -0500

    all logic in place

--- mummers/script.js
+++ mummers/script.js
@@ -1,162 +1,219 @@
-
-// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
-// const dayIDs = [];
-// for (var i = 0; i < numbers.length; i++) {
-//   let day = "day";
-//   let dateNumber = numbers[i];
-//   let oneDay = day.concat(dateNumber);
-//   oneDay = document.getElementById(oneDay);
-//   oneDay.addEventListener('click', openVideo(oneDay));
-//   dayIDs.push(oneDay);  
-// }
-
-
 // need to find where I disable things. Is there a style making the link invisible?
 // is something making the link unclickable? 
 
 // To "disable" a link, you can remove its href attribute, or add a click handler that returns false.
 
 
-var end = new Date('2021-11-17');
-var now = new Date();
-console.log(now);
-if (end - now <= 0) {
-    // var e = document.getElementById('locked');
-    // e.parentElement.removeChild(e);
-}
-
-
 let numbers = {
-  day1:{
-    dayID:"day1",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day1: {
+    day: new Date('November 1, 2021 00:00:01'),
+    dayID: "day1",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day2:{
-    dayID:"day2",
-    youtube:"https://www.youtube.com/embed/3t13XOceq0Q"
+  day2: {
+    day: new Date('November 2, 2021 00:00:01'),
+    dayID: "day2",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/3t13XOceq0Q"
   },
-  day3:{
-    dayID:"day3",
-    youtube:"https://www.youtube.com/embed/u_cLu4XM1uM"
+  day3: {
+    day: new Date('November 3, 2021 00:00:01'),
+    dayID: "day3",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/u_cLu4XM1uM"
   },
-  day4:{
-    dayID:"day4",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day4: {
+    day: new Date('November 4, 2021 00:00:01'),
+    dayID: "day4",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day5:{
-    dayID:"day5",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day5: {
+    day: new Date('November 5, 2021 00:00:01'),
+    dayID: "day5",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day6:{
-    dayID:"day6",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day6: {
+    day: new Date('November 6, 2021 00:00:01'),
+    dayID: "day6",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day7:{
-    dayID:"day7",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day7: {
+    day: new Date('November 7, 2021 00:00:01'),
+    dayID: "day7",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day8:{
-    dayID:"day8",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day8: {
+    day: new Date('November 8, 2021 00:00:01'),
+    dayID: "day8",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day9:{
-    dayID:"day9",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day9: {
+    day: new Date('November 9, 2021 00:00:01'),
+    dayID: "day9",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day10:{
-    dayID:"day10",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day10: {
+    day: new Date('November 10, 2021 00:00:01'),
+    dayID: "day10",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day11:{
-    dayID:"day11",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day11: {
+    day: new Date('November 11, 2021 00:00:01'),
+    dayID: "day11",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day12:{
-    dayID:"day12",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day12: {
+    day: new Date('November 12, 2021 00:00:01'),
+    dayID: "day12",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day13:{
-    dayID:"day13",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day13: {
+    day: new Date('November 13, 2021 00:00:01'),
+    dayID: "day13",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day14:{
-    dayID:"day14",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day14: {
+    day: new Date('November 14, 2021 00:00:01'),
+    dayID: "day14",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day15:{
-    dayID:"day15",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day15: {
+    day: new Date('November 15, 2021 00:00:01'),
+    dayID: "day15",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day16:{
-    dayID:"day16",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day16: {
+    day: new Date('November 16, 2021 00:00:01'),
+    dayID: "day16",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day17:{
-    dayID:"day17",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day17: {
+    day: new Date('November 17, 2021 00:00:01'),
+    dayID: "day17",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day18:{
-    dayID:"day18",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day18: {
+    day: new Date('November 18, 2021 00:00:01'),
+    dayID: "day18",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day19:{
-    dayID:"day19",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day19: {
+    day: new Date('November 19, 2021 00:00:01'),
+    dayID: "day19",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day20:{
-    dayID:"day20",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day20: {
+    day: new Date('November 20, 2021 00:00:01'),
+    dayID: "day20",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day21:{
-    dayID:"day21",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day21: {
+    day: new Date('November 21, 2021 00:00:01'),
+    dayID: "day21",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day22:{
-    dayID:"day22",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day22: {
+    day: new Date('November 22, 2021 00:00:01'),
+    dayID: "day22",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day23:{
-    dayID:"day23",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day23: {
+    day: new Date('November 23, 2021 00:00:01'),
+    dayID: "day23",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   },
-  day24:{
-    dayID:"day24",
-    youtube:"https://www.youtube.com/embed/LJMRu0UAYRg"
+  day24: {
+    day: new Date('November 24, 2021 00:00:01'),
+    dayID: "day24",
+    isVisible: false,
+    youtube: "https://www.youtube.com/embed/LJMRu0UAYRg"
   }
 };
 
-for (const number in numbers) {
-  let thisDay = numbers[number].dayID;
-  let thisVideo = numbers[number].youtube;
-  thisDay = document.getElementById(thisDay);
-  thisDay.addEventListener('click', openVideo());
+const today = new Date();
+
+
+function addEventListenersToDays(){
+  for (const number in numbers) {
+    let thisDay = numbers[number].dayID;
+    let thisVideo = numbers[number].youtube;
+    thisDay = document.getElementById(thisDay);
+    thisDay.addEventListener('click', openVideo());
+  }
+}
+addEventListenersToDays();
+
+dateInPastArrow = (firstDate, secondDate) => firstDate <= secondDate;
+
+
+function unBlurOpenDays (){
+  // loop through all the days
+  for (const number in numbers) {
+    // get the day ID
+    let thisDayID = numbers[number].dayID;
+    let thisDay = numbers[number].day;
+
+
+    // check if the firstDate is smaller than or equal to the second Date.
+    if (dateInPastArrow(thisDay, today)) {
+      let thisCalendarDay = document.getElementById(thisDayID);
+      let thisIcon = document.getElementById(thisDayID).firstElementChild;
+      // remove the blur class when needed
+      thisIcon.classList.remove("blurIcon");
+      // thisCalendarDay.classList.remove("inactiveTint");
+    }
+  }
+  // add the good hover class 
 }
 
+unBlurOpenDays();
+
+
 // sRxrwjOtIag
-function openVideo(e) {  
+function openVideo(e) {
   function functionThatReceivesAnEvent(event) {
-      for (const number in numbers) {
-      }
+
+    let clickedDayID = event.srcElement.closest("li").id;
+    let clickedDay = numbers[clickedDayID].day;
+
+    // check if the firstDate is smaller than or equal to the second Date.
+    if (dateInPastArrow(clickedDay, today)) {
+      // console.log("good")
       displayVidDiv(event);
+    }
   };
   return functionThatReceivesAnEvent;
 }
 
 let vidDiv = document.getElementById("vidDiv");
 
-function displayVidDiv(e){
-  // e.path doesn't work in firefox
-  console.log(e);
+function displayVidDiv(e) {
   let clickedDayID = e.srcElement.closest("li").id;
-
-  // let clickedDayID = e.path[1].id;
-  console.log(e.path);
-  // console.log(clickedDayID);
   let thisVideo = numbers[clickedDayID].youtube;
-  // console.log(thisVideo);
   vidDiv.classList.add("vidDiv");
 
-  vidDiv.innerHTML = "<iframe title='YouTube video player' type=\'text/html\' width='100%' height='100%' src=" + thisVideo + " frameborder='0' allowFullScreen></iframe>"
+  vidDiv.innerHTML = "<iframe title='YouTube video player' type=\'text/html\' width='90%' height='90%' src=" + thisVideo + " frameborder='0' allowFullScreen></iframe>"
   var a = document.createElement('a');
   a.classList.add("close");
   a.id = "closeButton";
@@ -166,10 +223,9 @@ function displayVidDiv(e){
   vidDiv.classList.add("visible");
 }
 
-function hideVidDiv(){
-  console.log("dgdf");
+function hideVidDiv() {
   vidDiv.innerHTML = null;
   vidDiv.classList.remove("vidDiv");
   vidDiv.classList.remove("visible");
-  closeButton.classList.remove("visible"); 
+  // closeButton.classList.remove("visible"); 
 }
